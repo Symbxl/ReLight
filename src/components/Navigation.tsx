654bf81d1132be05cc5bae12e953a31819472648
System Administrator <root@Users-MacBook-Pro.local>
2024-03-25 23:25:43 -0700

    done

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,10 @@
-import MenuNavigation from "./MenuNavigation";
 import MainNavigation from "./MainNavigation";
+import MenuNavigation from "./MenuNavigation";
 
 const Navigation = () => {
 
   return <>
-    <MenuNavigion />
+    <MenuNavigation />
     <MainNavigation />
   </>
 
